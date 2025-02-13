1dfedcf427dcfea37e55a1d2c8b025be2989b510
osegbu <valentineosegbu.com>
2025-02-13 19:14:28 +0100

    Debugging

--- src/components/PostContent/Poll.jsx
+++ src/components/PostContent/Poll.jsx
@@ -5,9 +5,10 @@ import Cookies from "js-cookie";
 import axios from "axios";
 
 const Poll = ({ post_id, total_votes, poll }) => {
-  const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
-  const token = Cookies.get("sessionData");
   const { state, updatePost } = useStore();
+  const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+  const token = Cookies.get("sessionData");
 
   const [loading, setLoading] = useState(false);
 
@@ -17,76 +18,63 @@ const Poll = ({ post_id, total_votes, poll }) => {
     return percentage.toFixed(0);
   };
 
-  const vote = async (poll_id) => {
+  const castVote = async (pollKey, is_voted) => {
+    if (!state.auth?.id || !pollKey) return;
+
     setLoading(true);
 
     try {
-      const response = await axios.post(
-        `${BASE_URL}/vote/${post_id}/${poll_id}`,
-        {},
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-      updatePost(response.data);
+      let response;
+      if (!is_voted) {
+        response = await axios.post(
+          `${BASE_URL}/vote/${post_id}/${poll.id}`,
+          {},
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+      } else {
+        response = await axios.delete(
+          `${BASE_URL}/vote/${post_id}/${poll.id}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+      }
+      // updatePost(response.data);
     } catch (error) {
-      // console.error(
-      //   "Error casting vote:",
-      //   error.response?.data || error.message
-      // );
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  const remove_vote = async (poll_id) => {
-    setLoading(true);
-
-    try {
-      const response = await axios.delete(
-        `${BASE_URL}/vote/${post_id}/${poll_id}`,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-      updatePost(response.data);
-    } catch (error) {
-      console.error(
-        "Error removing vote:",
-        error.response?.data || error.message
-      );
+      console.error(error.response?.data?.detail);
     } finally {
       setLoading(false);
     }
   };
 
   return (
-    <div
-      className={`${styles.pollDiv} ${poll.is_voted ? styles.active : ""}`}
-      onClick={(e) => {
-        if (loading) return;
-        if (poll.is_voted) {
-          remove_vote(poll.id);
-        } else {
-          vote(poll.id);
-        }
-        e.stopPropagation();
-      }}
-    >
-      <div className={styles.pollText}>
-        {loading && <div className={styles.loadingPoll}></div>}
-        {poll.option}
-      </div>
-      <div className={styles.voteCount}>{`${getVotePercent(poll.votes)}%`}</div>
+    <>
       <div
-        className={styles.voted}
-        style={{ width: `${getVotePercent(poll.votes)}%` }}
-      ></div>
-    </div>
+        className={`${styles.pollDiv} ${poll.is_voted ? styles.active : ""}`}
+        onClick={(e) => {
+          castVote(poll.id, poll.is_voted);
+          e.stopPropagation();
+        }}
+      >
+        <div className={styles.pollText}>
+          {loading && <div className={styles.loadingPoll}></div>}
+          {poll.option}
+        </div>
+        <div className={styles.voteCount}>
+          {`${getVotePercent(poll.votes)}%`}
+        </div>
+        <div
+          className={styles.voted}
+          style={{ width: `${getVotePercent(poll.votes)}%` }}
+        ></div>
+      </div>
+    </>
   );
 };
 
